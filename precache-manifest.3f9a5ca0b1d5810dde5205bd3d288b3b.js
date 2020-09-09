57dd4a21b2163d3627603c0773083eab670d955b
Squirrel <68919804+Roman-Leonchik@users.noreply.github.com>
2020-09-09 10:42:14 +0300

    First

---
+++ precache-manifest.3f9a5ca0b1d5810dde5205bd3d288b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe5553449aa16575d97ab164e24869ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74bea414a1319ad20065",
+    "url": "/static/css/main.37c739a7.chunk.css"
+  },
+  {
+    "revision": "ec3b165c5ba7adf12e06",
+    "url": "/static/js/2.aa5efaac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa5efaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74bea414a1319ad20065",
+    "url": "/static/js/main.fbbcff1f.chunk.js"
+  },
+  {
+    "revision": "f788c1d729ef6f66052b",
+    "url": "/static/js/runtime-main.eb19d4cf.js"
+  }
+]);
